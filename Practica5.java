e7bc7f0922a3d497119c34161a3b0a92c575db26
Neribb <neri.unam16@gmail.com>
2025-01-14 21:08:43 -0600

    Mi primer cambio

---
+++ Practica5.java
@@ -0,0 +1,18 @@
+List<Boolean> kidsWithCandies(List<Integer> candies, int extraCandies) {
+
+        List<Boolean> result = new ArrayList<>();
+        int greatest = 0;
+
+        for (int i = 0; i < candies.size(); i++) {
+            if (greatest <= candies.get(i)) {
+                greatest = candies.get(i)
+            }
+        }
+
+
+        for (int i = 0; i < candies.size(); i++) {
+
+            result.add(candies + extraCandies >= greatest);
+        }
+
+        return result;
